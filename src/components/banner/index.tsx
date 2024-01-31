1351d28804aa1b41c94fb3b0ae39627f9224b760
Abhishek3139 <abhishekchoudhary3139@gmail.com>
2024-01-31 09:05:27 +0530

    fix banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,12 +1,12 @@
 function Banner() {
   return (
     <section>
-      <div className=" text-white grid grid-cols-1 md:grid-cols-2 shop-now-background">
-        <div className="hidden md:block "></div>
+      <div className=" text-white grid grid-cols-1 md:grid-cols-2 bg-[#f2f2f2]">
+        <div className="hidden md:block shop-now-background"></div>
 
         <div className="text-center md:text-left p-16 my-auto">
-          <h1 className="text-4xl m-2 ">For The True</h1>
-          <h1 className="text-4xl m-2 ">Plant Lovers</h1>
+          <h1 className="text-4xl m-2 text-black">For The True</h1>
+          <h1 className="text-4xl m-2 text-black">Plant Lovers</h1>
           <button className="bg-green-700 w-36 h-10 m-4">Shop Now</button>
         </div>
       </div>
