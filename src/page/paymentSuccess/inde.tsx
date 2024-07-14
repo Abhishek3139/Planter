404fd034ca23805e4a0eee0cc89a02ddfcbdf3cb
Abhishek3139 <abhishek.choudhary@ensuesoft.com>
2024-07-14 22:13:56 +0530

    Update design

--- src/page/paymentSuccess/inde.tsx
+++ src/page/paymentSuccess/inde.tsx
@@ -1,5 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function PaymentSuccess() {
-  return <div>PaymentSuccess</div>;
+  const [deliveryInfo, setDeliveryInfo] = useState<any>();
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios('http://127.0.0.1:8000/api/v1/fedex/tracking');
+      setDeliveryInfo(response.data.data.output.completeTrackResults);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Your tracking number is :{deliveryInfo && deliveryInfo[0]?.trackingNumber}</h1>
+      <div>Delivey Status</div>
+      <table>
+        <tr>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+        {deliveryInfo &&
+          deliveryInfo[0]?.trackResults.map((track) => {
+            return track.scanEvents.reverse().map((eve, i) => {
+              return (
+                <tr key={i}>
+                  <td>{eve.eventDescription}</td>
+                  <td>{eve.derivedStatus}</td>
+                </tr>
+              );
+            });
+          })}
+      </table>
+    </div>
+  );
 }
 
 export default PaymentSuccess;
