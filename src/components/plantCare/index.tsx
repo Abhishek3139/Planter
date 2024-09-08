d1e318e1e2cb8d4c5833759e76208f762f34d61c
Abhishek3139 <abhishek.choudhary@ensuesoft.com>
2024-09-08 21:15:11 +0530

    fix img problem

--- src/components/plantCare/index.tsx
+++ src/components/plantCare/index.tsx
@@ -7,22 +7,22 @@ export function PlantCare() {
     <section className=''>
       <div className='my-24 mx-10 grid md:grid-cols-2  lg:grid-cols-4  '>
         <div className='my-10 flex flex-col justify-center items-center md:m-10'>
-          <img src='public/static/images/Group-1.png' />
+          <img src='/static/images/Group-1.png' />
           <h6 className='my-4'>Smart Plant & Tree Care</h6>
           <p>We want to help keep your plants ever-green.</p>
         </div>
         <div className='my-10 flex flex-col justify-center items-center md:m-10'>
-          <img src='public/static/images/Group-1.png' />
+          <img src='/static/images/Group-1.png' />
           <h6 className='my-4'>Smart Plant & Tree Care</h6>
           <p>We want to help keep your plants ever-green.</p>
         </div>
         <div className='my-10  flex flex-col justify-center items-center md:m-10'>
-          <img src='public/static/images/Group-1.png' />
+          <img src='/static/images/Group-1.png' />
           <h6 className='my-4'>Smart Plant & Tree Care</h6>
           <p>We want to help keep your plants ever-green.</p>
         </div>
         <div className='my-10 flex flex-col justify-center items-center md:m-10'>
-          <img src='public/static/images/Group-1.png' />
+          <img src='/static/images/Group-1.png' />
           <h6 className='my-4'>Smart Plant & Tree Care</h6>
           <p>We want to help keep your plants ever-green.</p>
         </div>
